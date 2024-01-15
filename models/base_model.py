ff58da7539b2b3cfbb31955a0083d45d8841311d
Malika <malikaoubilla@gmail.com>
2024-01-15 02:37:31 -0800

    storage issue

--- models/base_model.py
+++ models/base_model.py
@@ -2,6 +2,7 @@
 """This module defines a base class for all models in our hbnb clone"""
 import uuid
 from datetime import datetime
+from models import storage
 
 
 class BaseModel:
@@ -9,11 +10,9 @@ class BaseModel:
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
         if not kwargs:
-            from models import storage
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            storage.new(self)
         else:
             if 'id' not in kwargs:
                 kwargs['id'] = str(uuid.uuid4())
@@ -28,8 +27,12 @@ class BaseModel:
             else:
                 kwargs['created_at'] = datetime.now()
 
-            kwargs.pop('__class__', None)
-            self.__dict__.update(kwargs)
+            for key, value in kwargs.items():
+                if (key == "__class__"):
+                    continue
+                setattr(self, key, value)
+
+        storage.new(self)
 
     def __str__(self):
         """Returns a string representation of the instance"""
