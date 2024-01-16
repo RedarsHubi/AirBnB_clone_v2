8a541e9dc7ee2b9709269bb254de0c79e9252f03
root <redarshubi@github.com>
2024-01-16 05:15:50 -0800

    console error

--- models/base_model.py
+++ models/base_model.py
@@ -60,7 +60,7 @@ class BaseModel:
         dictionary['created_at'] = self.created_at.isoformat()
         dictionary['updated_at'] = self.updated_at.isoformat()
         if '_sa_instance_state' in dictionary.keys():
-            del my_dict['_sa_instance_state']
+            del dictionary['_sa_instance_state']
         return dictionary
 
     def delete(self):
