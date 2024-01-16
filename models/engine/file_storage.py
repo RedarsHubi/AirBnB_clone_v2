bc094fcdfd2554d398ca9daa240caebc23182225
root <redarshubi@github.com>
2024-01-16 15:04:08 -0800

    delete

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -13,7 +13,6 @@ class FileStorage:
 
     def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
-        self.reload()
         if cls is None:
             return FileStorage.__objects
         else:
