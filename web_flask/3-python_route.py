ec3316140e1118f4d200311e32ea6153bdf0de16
root <redarshubi@github.com>
2024-02-16 07:40:12 -0800

    3

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -23,6 +23,7 @@ def c_text(text):
     """ Function called with /c/<text> route """
     return 'C %s' % text.replace('_', ' ')
 
+
 @app.route('/python/', strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
 def py_text(text="is cool"):
