6eb7d902874d0ec8d4edc6bc1b545c70f8360280
root <redarshubi@github.com>
2024-02-16 09:12:39 -0800

    5

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -40,7 +40,7 @@ def numba(n):
 @app.route('/number_template/<int:n>', strict_slashes=False)
 def numb_temp(n):
     """ Function called with /number_template/<n> route """
-    return render_template("5-number.html", number = n)
+    return render_template("5-number.html", number=n)
 
 
 if __name__ == "__main__":
